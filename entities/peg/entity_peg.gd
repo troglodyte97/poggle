440de27a51d6585404c89f8950a3eea5b480f02c
troglodyte97 <kalsoder@outlook.com>
2025-11-17 10:09:47 +0100

    Removed previous iteration of peg entity

--- entities/peg/entity_peg.gd
+++ entities/peg/entity_peg.gd
@@ -1,45 +1,17 @@
-class_name EntityPegORIGINAL
+class_name EntityPeg
 extends RigidBody2D
 
-@export var main_sprite: Sprite2D
-@export var peg_sprites: Array[Texture2D]
+@export_group("Node Setup")
+@export var state_machine: StateMachine
 
+@export_subgroup("Effects")
 @export var animations: AnimationPlayer
+@export var hit_light: PointLight2D
+@export var particles: GPUParticles2D
 
-enum STATES { DEFAULT, ORANGE, HIT }
-var state: STATES = STATES.DEFAULT
+@export_subgroup("Sounds")
+@export var hit_sound: AudioStreamPlayer2D
 
-signal peg_hit(peg: EntityPeg)
-
-
-func _ready() -> void:
-	body_entered.connect(validate_collision)
-
-
-func change_state(new_state: STATES):
-	match new_state:
-		STATES.DEFAULT:
-			main_sprite.texture = peg_sprites[0]
-			state = STATES.DEFAULT
-		STATES.ORANGE:
-			main_sprite.texture = peg_sprites[1]
-			state = STATES.ORANGE
-		STATES.HIT:
-			main_sprite.texture = peg_sprites[2]
-			state = STATES.HIT
-			animations.play("hit_flash")
-			await get_tree().create_timer(1).timeout
-			queue_free()
-
-
-func validate_collision(collided_with: PhysicsBody2D):
-	if collided_with is EntityBall:
-		var ball = collided_with
-
-		if state != STATES.HIT:
-			ball.triggered_peg.emit(self)
-			peg_hit.emit(self)
-			change_state(STATES.HIT)
-
-		elif state == STATES.HIT:
-			ball.triggered_peg.emit(self)
+@export_subgroup("Sprites")
+@export var sprite_container: Node2D
+@export var main_sprite: Sprite2D
--- entities/peg/peg_state/states/orange.gd
+++
@@ -1,9 +0,0 @@
-extends PegState
-
-func enter(previous_state_path: String, data: Dictionary = { }) -> void:
-	peg.main_sprite.texture = self.state_texture
-	was_hit.connect(peg_was_hit)
-
-
-func peg_was_hit():
-	finished.emit(HIT)
