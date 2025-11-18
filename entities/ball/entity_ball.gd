489711f14d517453daa93d2badc5c516c0c21fb3
troglodyte97 <kalsoder@outlook.com>
2025-11-18 18:43:57 +0100

    Work on revisioned game mode, infinite fall down a grid.

--- entities/ball/entity_ball.gd
+++ entities/ball/entity_ball.gd
@@ -1,4 +1,17 @@
 class_name EntityBall
 extends RigidBody2D
 
+@export var velocity_limit: float = 400.0
+
+var gravity: float
+
 signal triggered_peg(peg: EntityPeg)
+
+
+func _ready() -> void:
+	gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if state.linear_velocity.y >= velocity_limit:
+		state.apply_central_force(Vector2(0, -state.linear_velocity.y))
